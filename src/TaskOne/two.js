1070bdd1ec6c0700133acff369de8b5fb44fe76b
Evgeny Korchagin <zhenek-com-post@mail.ru>
2020-08-03 09:25:42 +0400

    add babel

---
+++ src/TaskOne/two.js
@@ -0,0 +1,28 @@
+const chalk = require('chalk');
+const csv = require('csvtojson');
+const fs = require('fs');
+
+console.log(chalk.bgBlue("Start Tast One - Subtask Two"));
+
+const csvFilePath = 'src\\TaskOne\\input\\nodejs-hw1-ex1.csv';
+const txtFilePath = 'src\\TaskOne\\output.txt';
+
+const csvOptions = {
+  output: 'cvs',
+  noheader: false,
+  delimiter: [";"],
+  colParser: {
+    Book: "string",
+    Author: "string",
+    Amount: "omit",
+    Price: v => +v.replace(",", "."),
+  }
+}
+
+const readStream = fs.createReadStream(csvFilePath);
+const writeStream = fs.createWriteStream(txtFilePath);
+
+csv().on('error', err => console.log(err));
+csv(csvOptions)
+  .fromStream(readStream)
+  .subscribe(j => writeStream.write(JSON.stringify(j) + "\n"))
